9fefa33fe53590645902b1296e4bbad14877613e
Oskar Therén <oskartheren@gmail.com>
2016-04-25 16:11:59 +0200

    now i can run project both localy and on server. Time to start building

--- server.js
+++ server.js
@@ -13,8 +13,11 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
+process.env.NODE_CONFIG_DIR = './config/env';
+
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect('mongodb://localhost/mean-dev', function (err, database) { // local database
+// mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) { // heroku database
   if (err) {
     console.log(err);
     process.exit(1);
