80be11b51c5f337a3a38a05da042be697c5835eb
Oskar Therén <oskartheren@gmail.com>
2016-05-11 11:37:07 +0200

    Now everything works as intended, I will start working on the game now

--- public/javascripts/testApp.js
+++ public/javascripts/testApp.js
@@ -210,7 +210,6 @@ app.factory('auth', ['$http', '$window', function($http, $window){
 	  if(auth.isLoggedIn()){
 	    var token = auth.getToken();
 	    var payload = JSON.parse($window.atob(token.split('.')[1]));
-
 	    return payload.userName;
 	  }
 	};
