55d353d4768a157b92406d97abd8550dea7d70ca
Oskar Therén <oskartheren@gmail.com>
2016-05-13 13:28:08 +0200

    Added better collisiondetection, score, game over etc

--- public/javascripts/controllers/game.js
+++ public/javascripts/controllers/game.js
@@ -2,6 +2,7 @@ app.controller('GameCtrl', function($scope, users, auth){
   $scope.users = users.users;
   $scope.currentUser = auth.currentUser;
   $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.points = 0;
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
 
@@ -22,23 +23,16 @@ app.controller('GameCtrl', function($scope, users, auth){
 		}
 	};
 
-	$scope.createScoreOther = function(){ // For debugging
-		createScore($scope.pointsOther, $scope.userNameOther);
-		$scope.userNameOther = '';
-		$scope.pointsOther = '';
-	};
-	$scope.createScoreSelf = function(){
-		if(!$scope.isLoggedIn) return;
-		createScore($scope.pointsSelf, $scope.currentUser());
-		$scope.pointsSelf = '';
-	};
-	createScore = function(points, userName){
+	$scope.createScore = function(points, userName){
+    if(!$scope.isLoggedIn || !points) return;
 		var points = parseInt(points);
-		if(!points || !userName || userName === '') return;
 		users.createScore({points, userName});
+    $scope.points = '';
 	};
 
 	$scope.createEnemies = function() {
+    game.enemies.dir = 'right';
+    game.enemies.speed = 1;
 		var columns = 11;
 		var rows = 5;
 		for (i=0; i<columns; i++){
@@ -49,10 +43,10 @@ app.controller('GameCtrl', function($scope, users, auth){
 		}
 	};
 
-	$scope.shipMove = function(event) {
-		game.ship.pos.x = event.offsetX-game.ship.size.width;
-	};
-	$scope.shipShoot = function() {
+	// $scope.shipMove = function(event) {
+	// 	game.ship.pos.x = event.offsetX-game.ship.size.width;
+	// };
+	shipShoot = function() {
 		game.shots.shot.push({
 			pos: {
 				x:game.ship.pos.x+game.ship.size.width/2,
@@ -62,6 +56,21 @@ app.controller('GameCtrl', function($scope, users, auth){
 			speed: 5
 		});
 	};
+  $scope.keyboardHandler = function(event) {
+    console.log(event.code);
+    switch(event.code) {
+    case 'KeyA':
+      game.ship.pos.x -= 5;
+      break;
+    case 'KeyD':
+      game.ship.pos.x += 5;
+      break;
+    case 'KeyJ':
+      shipShoot();
+    default:
+        break;
+    }
+  };
 
 	draw = function(pos, size, fill, stroke) {
 		ctx.beginPath();
@@ -95,8 +104,10 @@ app.controller('GameCtrl', function($scope, users, auth){
 			if (game.enemies.dir == 'right') game.enemies.dir = 'left';
 			else game.enemies.dir = 'right'
 
-		  for (i=0; i<game.enemies.pos.length; i++)
+		  for (i=0; i<game.enemies.pos.length; i++){
         game.enemies.pos[i].y+=game.enemies.size.height*2;
+        if (game.enemies.pos[i].y > canvas.height-game.ship.size.height*4) alert('gameover');
+      }
 		}
 
 		for (i=0; i<game.enemies.pos.length; i++){
@@ -115,21 +126,34 @@ app.controller('GameCtrl', function($scope, users, auth){
     for (i=0; i<game.shots.shot.length; i++){
       for (j=0; j<game.enemies.pos.length; j++){
         if (
-          game.shots.shot[i].dir == 'up' && (
-            game.shots.shot[i].pos.x < game.enemies.pos[j].x &&
-            game.shots.shot[i].pos.x > game.enemies.pos[j].x - game.enemies.size.width
+          (
+            game.shots.shot[i].dir == 'up'
           ) && (
-            game.shots.shot[i].pos.y < game.enemies.pos[j].y &&
-            game.shots.shot[i].pos.y > game.enemies.pos[j].y - game.enemies.size.height
+            game.enemies.pos[j].x < game.shots.shot[i].pos.x + game.shots.size.width/2 &&
+            game.shots.shot[i].pos.x  + game.shots.size.width/2 < game.enemies.pos[j].x + game.enemies.size.width
+          ) && (
+            game.enemies.pos[j].y < game.shots.shot[i].pos.y &&
+            game.shots.shot[i].pos.y < game.enemies.pos[j].y + game.enemies.size.height
           )
         ){
+          $scope.points += 10
           game.shots.shot.splice(i, 1);
           game.enemies.pos.splice(j, 1);
+          enemiesHandleAmount();
+          break;
         }
       }
     }
   };
 
+  enemiesHandleAmount = function() {
+    if (game.enemies.pos.length == 0) $scope.createEnemies();
+    else if (game.enemies.pos.length < 2) game.enemies.speed = 10;
+    else if (game.enemies.pos.length < 4) game.enemies.speed = 7;
+    else if (game.enemies.pos.length < 15) game.enemies.speed = 4;
+    else if (game.enemies.pos.length < 25) game.enemies.speed = 3;
+    else if (game.enemies.pos.length < 40) game.enemies.speed = 2;
+  }
 
 
 	setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
