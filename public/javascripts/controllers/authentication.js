1d98af8277e21a3d88de8aa91d290c6bf8c4b71d
Oskar Therén <oskartheren@gmail.com>
2016-05-23 12:22:03 +0200

    Now correct error message is shown when regestrating an existing username and made some clientside registation rules

--- public/javascripts/controllers/authentication.js
+++ public/javascripts/controllers/authentication.js
@@ -3,11 +3,19 @@ app.controller('AuthCtrl', function($scope, $state, auth){
   $scope.loginUser = {};
 
   $scope.register = function(){
-    auth.register($scope.registerUser).error(function(error){
-      $scope.error = error;
-    }).then(function(){
-      $state.go('high_score');
-    });
+		if ($scope.registerUser.password != $scope.registerUser.passwordRepeat){
+			$scope.error = {message:"Passwords does not match"};
+		} else if ($scope.registerUser.password.length < 7) {
+			$scope.error = {message:"Password must be atleast 6 characters long"};
+		}
+		else {
+	    auth.register($scope.registerUser).error(function(error){
+	      $scope.error = error;
+	    }).then(function(){
+	      $state.go('high_score');
+	    });
+		}
+		console.log("$scope.error", $scope.error);
   };
 
   $scope.logIn = function(){
--- routes/index.js
+++ routes/index.js
@@ -73,14 +73,18 @@ router.post('/register', function(req, res, next){
   if(!req.body.userName || !req.body.password){
     return res.status(400).json({message: 'Please fill out all fields'});
   }
-  var user = new User();
-  user.userName = req.body.userName;
-  user.setPassword(req.body.password)
+  var query = User.find({'userName' : req.body.userName});
+  query.exec(function (err, user){
+    if (err) { return next(err); }
+    if (user[0]) { return res.status(401).json({message: 'Username already exists'}) }
+    var user = new User();
+    user.userName = req.body.userName;
+    user.setPassword(req.body.password)
 
-  user.save(function (err){
-    if(err){ return next(err); }
-
-    return res.json({token: user.generateJWT()})
+    user.save(function (err){
+      if(err){ return next(err); }
+      return res.json({token: user.generateJWT()})
+    });
   });
 });
 
